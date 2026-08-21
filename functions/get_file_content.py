7d603e4db8b1549f4b3cd9c19bd03a68b2458b34
lucasmarques2907 <lucasredra@gmail.com>
2026-08-21 13:18:04 -0300

    chore: changing variable names

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,15 +4,15 @@ from config import MAX_CHARS
 def get_file_content(working_directory: str, file_path: str) -> str:
     try:
         working_dir_abs = os.path.abspath(working_directory)
-        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
-        valid_file_path = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        file_path_abs = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_file_path = os.path.commonpath([working_dir_abs, file_path_abs]) == working_dir_abs
     
         if not valid_file_path:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(target_file):
+        if not os.path.isfile(file_path_abs):
             return f'Error: File not found or is not a regular file: "{file_path}"'
     
-        with open(target_file, "r") as f:
+        with open(file_path_abs, "r") as f:
             content = f.read(MAX_CHARS)
             if f.read(1):
                 content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
