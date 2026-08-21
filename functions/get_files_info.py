57f92d44e2ae726aef8d5a547b51fde60e9ee218
lucasmarques2907 <lucasredra@gmail.com>
2026-08-21 10:35:59 -0300

    feat: listing files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,8 +8,18 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        if not os.path.isdir(directory):
+        if not os.path.isdir(working_dir_abs):
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'    
+        
+        files_info: list[str] = []
+        for filename in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(file_path)
+            file_size = os.path.getsize(file_path)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        
+        return "\n".join(files_info)
     except Exception as err:
         return f"Error: {err}"
